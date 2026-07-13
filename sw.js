1119c37b84e9c948dc3e5186530240783af2fdfe
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-13 23:49:54 +0200

    Strengthen map links with backup actions

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-ryczywol-";
-const CACHE_NAME = CACHE_PREFIX + "20260713-ryczywol-moduleunified3";
+const CACHE_NAME = CACHE_PREFIX + "20260713-ryczywol-linkguard1";
 
 const CORE_ASSETS = [
   "./",
@@ -18,10 +18,10 @@ const CORE_ASSETS = [
   "./zakazy.html",
   "./test.html",
   "./manifest.webmanifest",
-  "./assets/css/training.css?v=20260713-ryczywol-moduleunified3",
+  "./assets/css/training.css?v=20260713-ryczywol-linkguard1",
   "./assets/js/training-data.js?v=20260713-ryczywol-lekarz1",
   "./assets/js/location-custom.js?v=20260713-ryczywol-moduleunified3",
-  "./assets/js/training-app.js?v=20260713-ryczywol-moduleunified3",
+  "./assets/js/training-app.js?v=20260713-ryczywol-linkguard1",
   "./assets/brand/polskie-pomidory-logo.png",
   "./assets/brand/polskie-pomidory-icon.png",
   "./assets/orientation/sklarnia-etap-excel.png"
