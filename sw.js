362db96e8fefcec30dee8e44d11c6f620cbd2ff3
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-13 13:13:13 +0200

    Dodaj przycisk Hydra

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-ryczywol-";
-const CACHE_NAME = CACHE_PREFIX + "20260713-contacts1-ryczywol";
+const CACHE_NAME = CACHE_PREFIX + "20260713-hydra-link1-ryczywol";
 
 const CORE_ASSETS = [
   "./",
@@ -18,10 +18,10 @@ const CORE_ASSETS = [
   "./zakazy.html",
   "./test.html",
   "./manifest.webmanifest",
-  "./assets/css/training.css?v=20260713-ryczywol-lekarz1",
+  "./assets/css/training.css?v=20260713-hydra-link1-ryczywol",
   "./assets/js/training-data.js?v=20260713-ryczywol-lekarz1",
   "./assets/js/location-custom.js?v=20260713-contacts1-ryczywol",
-  "./assets/js/training-app.js?v=20260713-contacts1-ryczywol",
+  "./assets/js/training-app.js?v=20260713-hydra-link1-ryczywol",
   "./assets/brand/polskie-pomidory-logo.png",
   "./assets/brand/polskie-pomidory-icon.png",
   "./assets/orientation/sklarnia-etap-excel.png"
