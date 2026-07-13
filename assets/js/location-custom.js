20ed4e63eb0dabffc1642a193b3865d6b2b0c260
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-13 23:10:48 +0200

    Unify training module visuals

--- assets/js/location-custom.js
+++ assets/js/location-custom.js
@@ -3,7 +3,7 @@
   if (!D) return;
   const T = {
   "meta": {
-    "version": "20260713-ryczywol-unifiedstyle3",
+    "version": "20260713-ryczywol-moduleunified3",
     "displayVersion": "2026-07-13",
     "location": "Ryczywół k. Kozienic",
     "appTitle": "Polskie Pomidory Ryczywół k. Kozienic",
