ff26aa121cc13d0bd2f6369d1ceaa1b43fe7f8a3
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-13 15:45:44 +0200

    Ulepsz styl mobilny Ryczywol

--- assets/js/location-custom.js
+++ assets/js/location-custom.js
@@ -3,7 +3,7 @@
   if (!D) return;
   const T = {
   "meta": {
-    "version": "20260713-ryczywol-data4",
+    "version": "20260713-ryczywol-data5",
     "displayVersion": "2026-07-13",
     "location": "Ryczywół k. Kozienic",
     "appTitle": "Polskie Pomidory Ryczywół k. Kozienic",
